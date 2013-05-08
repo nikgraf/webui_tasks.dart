8771eef13f25a983502aa04fb60fe6924fe7f5c3
Damon Douglas <douglas.damon@gmail.com>
2013-05-07 23:25:50 -0400

    Fixed copy out task to handle multiple calls.

--- lib/webui_tasks.dart
+++ lib/webui_tasks.dart
@@ -8,7 +8,9 @@ import 'package:hop/hop_tasks.dart';
 import 'package:html5lib/parser.dart';
 import 'package:html5lib/dom.dart';
 
-// Adapted from CompilerTargetType courtesy of:  https://github.com/kevmoo/hop.dart/blob/master/lib/src/hop_tasks/dart2js.dart
+/* 
+ * Adapted from [CompilerTargetType] courtesy of:  https://github.com/kevmoo/hop.dart/blob/master/lib/src/hop_tasks/dart2js.dart
+*/
 class WebuiTargetType {
   final String _value;
   const WebuiTargetType._internal(this._value);
@@ -18,6 +20,14 @@ class WebuiTargetType {
   static const DART = const WebuiTargetType._internal('dart');
   static const MINIDART = const WebuiTargetType._internal('minidart');
 }
+
+/*
+ * Wrapper for the DWC compiler.
+ * [entryPoint] is the path to the main entry point of the webui application.  
+ * [outputPath] defaults to [project_root/output] unless one is provided.  
+ * [rewriteUrls] determines whether to pass --rewrite-urls or --no-rewrite-urls to the dwc compiler.
+ * [outputType] can be either [WebuiTargetType.DART] or [WebuiTargetType.MINIDART].
+ */
 
 Task createWebui2DartTask(String entryPoint, {String outputPath:"output", bool rewriteUrls:false, WebuiTargetType outputType:WebuiTargetType.DART}) {
   requireArgument(outputType == WebuiTargetType.MINIDART || outputType == WebuiTargetType.DART, 'outputType');
@@ -37,6 +47,12 @@ Task createWebui2DartTask(String entryPoint, {String outputPath:"output", bool r
   });
 }
 
+/*
+ * Copies linked static assets referenced from the main entry point of the webui application to the [outputPath].
+ * [entryPoint] is the path to the main entry point of the webui application.  
+ * [outputPath] defaults to [project_root/output] unless one is provided. 
+ */
+
 Task createCopyOutTask(String entryPoint, {String outputPath:"output"}) {
   final entryPointPath = new Path(entryPoint);
   final entryPointFile = new File.fromPath(entryPointPath);
@@ -49,7 +65,24 @@ Task createCopyOutTask(String entryPoint, {String outputPath:"output"}) {
   });
 }
 
+/*
+ * Fixes urls to static assets to point to the output directory as relative urls.
+ * [entryPoint] is the path to the main entry point of the webui application.  
+ * [outputPath] defaults to [project_root/output] unless one is provided.
+ * [outputType] is of type [WebuiTargetType] and determines the extension of the bootstrap script.
+ */
+
 Task createFixUrlTask(String entryPoint, {String outputPath:"output", WebuiTargetType outputType:WebuiTargetType.DART}) {
+  
+}
+
+
+Future<bool> _fixUrls(TaskContext ctx, String output, String sourceDir, String entryPointFileName) {
+  var entryPoint = new File("$sourceDir/$entryPointFileName");
+  var outputPath = new Path(output);
+  
+  assert(entryPoint.existsSync());
+  
   
 }
 
@@ -88,7 +121,7 @@ Future<bool> _copy_out(TaskContext ctx, String output, String sourceDir, String 
   assert(entryPoint.existsSync());
   
   var document = parse(entryPoint.readAsStringSync());
-  var urlMap = {};
+  var assetList = [];
   
   document
   .queryAll("script")
@@ -101,36 +134,20 @@ Future<bool> _copy_out(TaskContext ctx, String output, String sourceDir, String 
         var asset = assetFile.readAsStringSync();
         var copyFile = new File.fromPath(outputPath.append(assetPath.filename));
         copyFile.writeAsStringSync(asset);
-        urlMap[src] = assetPath.filename;
+        assetList.add(assetPath.filename);
     } 
   });
   
   entryPoint = new File("$output/$entryPointFileName");
   document = parse(entryPoint.readAsStringSync());
   
-  document
-  .queryAll("script")
-  .forEach((element){
-    String src = element.attributes["src"];
-    
-    if(!src.contains(".dart")) {
-      element.attributes["src"] = urlMap[src];
-    }
-  });
-  
-  entryPoint.writeAsStringSync(document.outerHtml);
-  
   var success = true;
-  urlMap
-  .values
+  
+  assetList
   .forEach((assetName){
-    success = success && new File("$output/$assetName").existsSync();  
-  });  
+    success = success && new File("$output/$assetName").existsSync();
+  });
   
   completer.complete(success);
   return completer.future;
-}
-
-Future<bool> _fixUrls(TaskContext ctx, String outputEntryPoint) {
-  
 }
